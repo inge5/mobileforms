55ddcd0d92febaafe114ba1b4ec0f41aa352ad2f
Nelson Zambrano <inge5neuromedia@gmail.com>
2022-05-04 23:34:45 -0500

    ultimos cambios

--- src/app/mobileforms/mobileforms.component.ts
+++ src/app/mobileforms/mobileforms.component.ts
@@ -1,24 +1,40 @@
 import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
 
-import { DomSanitizer, SafeHtml, SafeResourceUrl, SafeScript, SafeStyle, SafeUrl } from '@angular/platform-browser';
+import {
+  DomSanitizer,
+  SafeHtml,
+  SafeResourceUrl,
+  SafeScript,
+  SafeStyle,
+  SafeUrl,
+} from '@angular/platform-browser';
 import { HomeService } from '../services/home.service';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { CommonService } from '../services/common.service';
 
-declare var $ : any; 
+declare var $: any;
 
-@Pipe({ name: 'safeHtml'})
-export class SafeHtmlPipe implements PipeTransform  {
+@Pipe({ name: 'safeHtml' })
+export class SafeHtmlPipe implements PipeTransform {
   constructor(private sanitized: DomSanitizer) {}
-  public transform(value: any, type: string): SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
+  public transform(
+    value: any,
+    type: string
+  ): SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
     // return this.sanitized.bypassSecurityTrustHtml(value);
     switch (type) {
-      case 'html': return this.sanitized.bypassSecurityTrustHtml(value);
-      case 'style': return this.sanitized.bypassSecurityTrustStyle(value);
-      case 'script': return this.sanitized.bypassSecurityTrustScript(value);
-      case 'url': return this.sanitized.bypassSecurityTrustUrl(value);
-      case 'resourceUrl': return this.sanitized.bypassSecurityTrustResourceUrl(value);
-      default: throw new Error(`Invalid safe type specified: ${type}`);
+      case 'html':
+        return this.sanitized.bypassSecurityTrustHtml(value);
+      case 'style':
+        return this.sanitized.bypassSecurityTrustStyle(value);
+      case 'script':
+        return this.sanitized.bypassSecurityTrustScript(value);
+      case 'url':
+        return this.sanitized.bypassSecurityTrustUrl(value);
+      case 'resourceUrl':
+        return this.sanitized.bypassSecurityTrustResourceUrl(value);
+      default:
+        throw new Error(`Invalid safe type specified: ${type}`);
     }
   }
 }
@@ -26,49 +42,55 @@ export class SafeHtmlPipe implements PipeTransform  {
 @Component({
   selector: 'app-mobileforms',
   templateUrl: './mobileforms.component.html',
-  styleUrls: ['./mobileforms.component.css']
+  styleUrls: ['./mobileforms.component.css'],
 })
 export class MobileformsComponent implements OnInit {
-
   public userside: any;
-  data:any = [];
+  data: any = [];
   loader = true;
-  submenu:any = [];
+  submenu: any = [];
 
-  constructor(private _sanitizer: DomSanitizer, private _homeservice:HomeService, private common: CommonService) { 
-    
-  }
+  constructor(
+    private _sanitizer: DomSanitizer,
+    private _homeservice: HomeService,
+    private common: CommonService
+  ) {}
 
   ngOnInit(): void {
     this.common.paginaMobileFormsMetaData();
-    this._homeservice.getHomeMobileForms()
-    .subscribe((res:any) => {
+    this._homeservice.getHomeMobileForms().subscribe((res: any) => {
       this.loader = false;
       this.data = this._sanitizer.bypassSecurityTrustHtml(res);
       this.data = this.data.changingThisBreaksApplicationSecurity;
     });
   }
+  colocarColor(id: number) {
+    $(`#${id}`).toggleClass('colorCliente');
+  }
 
   reason = '';
 
-  abrirSide(){
-    $("#wrapper").toggleClass("toggled");
+  abrirSide() {
+    $('#wrapper').toggleClass('toggled');
     $('.overlaytrabaja').addClass('active');
   }
 
   public cierraTrabajemos() {
     $('.overlaytrabaja').removeClass('active');
-    $("#wrapper").toggleClass("toggled");
+    $('#wrapper').toggleClass('toggled');
   }
-  
-  scrollConClick( url:string ){
-    console.log(url);
-    $('html, body').animate({
-      scrollTop: $(url).offset().top
-    }, .5);
-}
 
-   customOptions: OwlOptions = {
+  scrollConClick(url: string) {
+    console.log(url);
+    $('html, body').animate(
+      {
+        scrollTop: $(url).offset().top,
+      },
+      0.5
+    );
+  }
+
+  customOptions: OwlOptions = {
     loop: true,
     mouseDrag: true,
     touchDrag: true,
@@ -82,19 +104,18 @@ export class MobileformsComponent implements OnInit {
     navText: ['', ''],
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       400: {
-        items: 2
+        items: 2,
       },
       740: {
-        items: 3
+        items: 3,
       },
       940: {
-        items: 8
-      }
+        items: 8,
+      },
     },
-    nav: false
-  }
-
+    nav: false,
+  };
 }
